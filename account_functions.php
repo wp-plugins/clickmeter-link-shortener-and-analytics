f713a0991947b2b940fd4b588f8c2415636106dd
ClickMeter <ClickMeter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-28 16:47:45 +0000

    v.1.2.4.2
    git-svn-id: https://plugins.svn.wordpress.org/clickmeter-link-shortener-and-analytics/tags/1.2.4.2@1034584 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ account_functions.php
@@ -0,0 +1,404 @@
+<?php
+require_once( plugin_dir_path( __FILE__ ) . 'clickmeter.php');
+require_once( plugin_dir_path( __FILE__ ) . 'clickmeter_views.class.php');
+
+add_thickbox(); //enable opening modal popup
+
+//API KEY VALIDATION
+$api_key = "";
+
+$args = array(
+'posts_per_page' => -1,
+'post_type' => 'post',
+'post_status' => array('publish', 'private', 'future'),
+'orderby' => 'title',
+'order' => 'ASC'
+);
+$posts_array = get_posts( $args );
+
+$args = array(
+'posts_per_page' => -1,
+'post_type' => 'page',
+'post_status' => array('publish', 'private', 'future'),
+'orderby' => 'title',
+'order' => 'ASC'
+);
+$pages_array = get_posts( $args );
+
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+//CODE FOR ACCOUNT!
+if ($_SERVER["REQUEST_METHOD"] == "POST" && array_key_exists("API_key",$_POST)) {
+	if (empty($_POST["API_key"])) {
+		$apikeyErr = "*API-Key is required";
+	} else {
+		$api_key = test_input($_POST["API_key"]);
+		$jsonoutput = WPClickmeter::api_request('http://apiv2.clickmeter.com/account', 'GET', NULL, $api_key);
+		WPClickmeter::store_option( 'clickmeter_debug_init', $jsonoutput);		
+		if (!preg_match("/^[a-zA-Z0-9]{8}\-[a-zA-Z0-9]{4}\-[a-zA-Z0-9]{4}\-[a-zA-Z0-9]{4}\-[a-zA-Z0-9]{12}$/",$api_key)) {
+			$apikeyErr = "*Invalid API-Key"; 
+		}if(empty($jsonoutput)) {
+			$apikeyErr = "*Invalid API-Key"; 
+		}
+		else{
+			WPClickmeter::store_option( 'clickmeter_api_key', $api_key);
+			$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/account', 'GET', NULL, $api_key);
+			$boGoVal = $json_output[boGoVal];
+			if($boGoVal!=NULL) WPClickmeter::store_option( 'clickmeter_backOffice_key', $boGoVal);
+
+			$blog_name = get_site_url();
+			$blog_name = substr($blog_name,7);
+
+			//look for TP campaign into WP database
+			$group_id_TP = WPClickmeter::get_option('clickmeter_TPcampaign_id');
+			//if not present
+			if($group_id_TP==null){
+				//search it into clickmeter and save it
+				$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com:80/groups?status=active&_expand=true', 'GET', NULL, $api_key);
+				$groups = $json_output[entities];
+				foreach ($groups as $group) {
+					if(strcasecmp($group[name], $blog_name.'-views')==0){
+						$group_id_TP = $group[id];
+						WPClickmeter::store_option( 'clickmeter_TPcampaign_id', $group_id_TP );	
+						break;
+					} 
+				}
+				//if not present in clickmeter, create it
+				if($group_id_TP == null){
+					//Create campaign for wordpress pixels
+					$body=array('name'=>$blog_name.'-views');
+					$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/groups','POST', json_encode($body), $api_key);
+					$group_id_TP = $json_output[id];
+					WPClickmeter::store_option( 'clickmeter_TPcampaign_id', $group_id_TP );
+				}
+			}
+
+			//look for TL campaign into WP database
+			$group_id_TL = WPClickmeter::get_option('clickmeter_TLcampaign_id');
+			//if not present
+			if($group_id_TL==null){
+				//search it into clickmeter and save it
+				$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com:80/groups?status=active&_expand=true', 'GET', NULL, $api_key);
+				$groups = $json_output[entities];
+				foreach ($groups as $group) {
+					if(strcasecmp($group[name],$blog_name.'-links')==0){
+						$group_id_TL = $group[id];
+						WPClickmeter::store_option( 'clickmeter_TLcampaign_id', $group_id_TL );	
+						break;	
+					} 
+				}
+				//if not present in clickmeter, create it
+				if($group_id_TL == null){
+					//Create campaign for wordpress tracking links
+					$body=array('name'=>$blog_name.'-links');
+					$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/groups','POST', json_encode($body), $api_key);
+					$group_id_TL = $json_output[id];
+					WPClickmeter::store_option( 'clickmeter_TLcampaign_id', $group_id_TL );
+				}
+			}
+
+			//look for 404_reports campaign into WP database
+			$group_id_404_reports = WPClickmeter::get_option('clickmeter_404_reports_campaign_id');
+			//if not present
+			if($group_id_404_reports==null){
+				//search it into clickmeter and save it
+				$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com:80/groups?status=active&_expand=true', 'GET', NULL, $api_key);
+				$groups = $json_output[entities];
+				foreach ($groups as $group) {
+					if(strcasecmp($group[name],$blog_name.'-404 reports')==0){
+						$group_id_404_reports = $group[id];
+						WPClickmeter::store_option( 'clickmeter_404_reports_campaign_id', $group_id_404_reports );	
+						break;	
+					} 
+				}
+				//if not present in clickmeter, create it
+				if($group_id_404_reports == null){
+					//Create campaign for wordpress tracking links
+					$body=array('name'=>$blog_name.'-404 reports');
+					$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/groups','POST', json_encode($body), $api_key);
+					$group_id_404_reports = $json_output[id];
+					WPClickmeter::store_option( 'clickmeter_404_reports_campaign_id', $group_id_404_reports );
+				}
+			}
+			//WPClickmeter::store_option( 'clickmeter_startup_create_TP', $_POST["startup_create_TP"]);
+			if($_POST["startup_create_TP"]=="false"){
+					WPClickmeter::store_option("clickmeter_workinprogress_flag", "false");
+					WPClickmeter::store_option( 'clickmeter_pixel_flag', 0 );
+					WPClickmeter::store_option('clickmeter_pixel_new_articles', 1 );
+					WPClickmeter::store_option( 'clickmeter_inclusion_list', array());
+					WPClickmeter::store_option( 'clickmeter_exclusion_list', array());
+					WPClickmeter::store_option("track_404_flag", 0);
+					WPClickmeter::store_option("link_cloak_flag", 0);
+					WPClickmeter::store_option("clickmeter_wp_redirection_flag", 0);
+					WPClickmeter::store_option("clickmeter_default_redirection_type", "307");
+					echo '<meta http-equiv="refresh" content="0">';
+
+			}elseif($_POST["startup_create_TP"]=="true"){
+				$inclusion_list = array();
+				foreach ($posts_array as $post) {
+					$inclusion_list[] = $post->ID;
+				}
+				foreach ($pages_array as $page) {
+					$inclusion_list[] = $page->ID;
+				}				
+				WPClickmeter::store_option( 'clickmeter_inclusion_list', $inclusion_list);
+				WPClickmeter::store_option( 'clickmeter_pixel_flag', 1 );
+				WPClickmeter::store_option('clickmeter_pixel_new_articles', 1 );
+				WPClickmeter::store_option("track_404_flag", 0);
+				WPClickmeter::store_option("link_cloak_flag", 0);
+				WPClickmeter::store_option("clickmeter_wp_redirection_flag", 0);
+				WPClickmeter::store_option("clickmeter_default_redirection_type", "307");
+				echo '<meta http-equiv="refresh" content="0">';
+				echo '<script>window.location.replace("?page=clickmeter-link-shortener-and-analytics/view/clickmeter-loading_tracking_pixels_ops.php&startup_create_TP=true");</script>';
+			}
+
+
+			// //I don't have conversion ids in the database, search on ClickMeter by blog name.
+			// $active_conversions = WPClickmeter::api_request('http://apiv2.clickmeter.com/conversions?status=active&_expand=true', 'GET', NULL, $api_key);
+			// $conversion1_name = "";
+			// //conversion 1
+			// foreach ($active_conversions[entities] as $conversion) {
+			// 	if((preg_match("#".$blog_name."#", $conversion[name]) && $conversion2_name!=$conversion[name])){
+			// 		WPClickmeter::store_option("clickmeter_conversionId1", $conversion[id]);
+			// 		WPClickmeter::store_option("clickmeter_conversionName1", $conversion[name]);
+			// 		WPClickmeter::store_option("clickmeter_conversionTarget1", array("post", "page"));
+			// 		$conversion1_name = $conversion[name];
+			// 	}
+			// }
+
+			// //conversion 2
+			// foreach ($active_conversions[entities] as $conversion) {
+			// 	if((preg_match("#".$blog_name."#", $conversion[name]) && $conversion1_name!=$conversion[name])){
+			// 		WPClickmeter::store_option("clickmeter_conversionId2", $conversion[id]);
+			// 		WPClickmeter::store_option("clickmeter_conversionName2", $conversion[name]);
+			// 		WPClickmeter::store_option("clickmeter_conversionTarget2", array("post", "page"));
+			// 	}
+			// }
+		}
+	}
+}
+
+$api_key=WPClickmeter::get_option('clickmeter_api_key');
+
+if($api_key!=NULL){
+	function hideAPIKey($value) {
+		$maskedValue = $value;
+		$lenght = strlen($maskedValue);
+		for($i=3;$i<$lenght-3;$i++){
+			$maskedValue[$i]='*';
+		}
+		return $maskedValue;
+	}
+	
+	$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/account', 'GET', NULL, $api_key);
+	$username = $json_output[firstName];
+	$lastname = $json_output[lastName];
+	$email = $json_output[email];
+	$companyName = $json_output[companyName];
+	$companyRole = $json_output[companyRole];
+	$phone = $json_output[phone];
+	$boGoVal = $json_output[boGoVal];
+
+	$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/account/plan', 'GET', NULL, $api_key);
+	$plan_type = $json_output[name];
+	$maximumDatapoints = $json_output[maximumDatapoints];
+	$used_datapoints = $json_output[usedDatapoints];
+	$monthlyEvents = $json_output[monthlyEvents];
+	$billingPeriodStart = $json_output[billingPeriodStart];
+	$billingPeriodStart = substr($billingPeriodStart,0,4).'/'.substr($billingPeriodStart,4,2).'/'.substr($billingPeriodStart,6,2);
+	$billingPeriodEnd = $json_output[billingPeriodEnd];
+	$billingPeriodEnd = substr($billingPeriodEnd,0,4).'/'.substr($billingPeriodEnd,4,2).'/'.substr($billingPeriodEnd,6,2);
+	$usedMonthlyEvents = $json_output[usedMonthlyEvents];
+
+	$datapoints_flag = true;
+	$available_datapoints = $maximumDatapoints - $used_datapoints;
+	$already_created_pixel = WPClickmeter::get_pixel_count();
+
+	//GET TRACKING PIXELS LIST
+	$group_id_TP = WPClickmeter::get_option('clickmeter_TPcampaign_id');
+	$group_id_TL = WPClickmeter::get_option('clickmeter_TLcampaign_id');
+	$group_id_404_reports = WPClickmeter::get_option('clickmeter_404_reports_campaign_id');
+
+	//CONVERSIONS
+	$conversion1_id = WPClickmeter::get_option('clickmeter_conversionId1');
+	$conversion1_name = WPClickmeter::get_option("clickmeter_conversionName1");
+	$conversion2_id = WPClickmeter::get_option('clickmeter_conversionId2');
+	$conversion2_name = WPClickmeter::get_option("clickmeter_conversionName2");
+	
+
+	//Check for existing conversions in ClickMeter
+	//Case 1: I have a conversion ids in the database and I search it on ClickMeter
+	if($conversion1_id!=null){
+		$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/conversions/'.$conversion1_id, 'GET', NULL, $api_key);
+		if(!array_key_exists("deleted",$json_output)){
+			WPClickmeter::store_option("clickmeter_conversionName1", $json_output[name]);
+			$conversion1_name = $json_output[name];
+		}
+	}
+	if($conversion2_id!=null){
+		$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/conversions/'.$conversion2_id, 'GET', NULL, $api_key);
+		if(!array_key_exists("deleted",$json_output)){
+			WPClickmeter::store_option("clickmeter_conversionName2", $json_output[name]);
+			$conversion2_name = $json_output[name];
+		}
+	}
+	
+	//Case 2: I don't have conversion ids in the database, search on ClickMeter by blog name.
+	$blog_name = get_site_url();
+	$blog_name = substr($blog_name,7);
+	$active_conversions = WPClickmeter::api_request('http://apiv2.clickmeter.com/conversions?status=active&_expand=true', 'GET', NULL, $api_key);
+
+	$conversion1_id = WPClickmeter::get_option('clickmeter_conversionId1');
+	$conversion1_name = WPClickmeter::get_option("clickmeter_conversionName1");
+	$conversion1_target = WPClickmeter::get_option("clickmeter_conversionTarget1");
+	$conversion2_id = WPClickmeter::get_option('clickmeter_conversionId2');
+	$conversion2_name = WPClickmeter::get_option("clickmeter_conversionName2");
+	$conversion2_target = WPClickmeter::get_option("clickmeter_conversionTarget2");
+
+	$pixel_value=WPClickmeter::get_option('clickmeter_pixel_flag');
+	$pixel_default_value = WPClickmeter::get_option('clickmeter_pixel_new_articles');	
+	$exclusion_list = WPClickmeter::get_option("clickmeter_exclusion_list");
+	$inclusion_list = WPClickmeter::get_option("clickmeter_inclusion_list");
+	
+
+	//TRACKING LINK SETTINGS
+	
+	//DOMAINS
+	$wordpress_domain_id = 1597;
+	$default_domainId = WPClickmeter::get_option("clickmeter_default_domainId");
+	if($_POST['domain_list']!=null){
+		$default_domainId = $_POST['domain_list'];
+		WPClickmeter::store_option("clickmeter_default_domainId", $default_domainId);
+		if($default_domainId == $wordpress_domain_id){
+			WPClickmeter::store_option("clickmeter_default_domainName", $blog_name);
+		}else{
+			$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com:80/domains/'.$default_domainId, 'GET', NULL, $api_key);
+			WPClickmeter::store_option("clickmeter_default_domainName", $json_output[name]);	
+		}
+	}
+	
+	$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/domains?_expand=true', 'GET', NULL, $api_key);
+	$clickmeter_domains = $json_output[entities];
+	$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/domains?type=go&_expand=true', 'GET', NULL, $api_key);
+	foreach ($json_output[entities] as $goDomain) {
+		$clickmeter_domains[] = $goDomain;
+	}
+	$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/domains?type=personal&_expand=true', 'GET', NULL, $api_key);
+	foreach ($json_output[entities] as $personalDomain) {
+		$clickmeter_domains[] = $personalDomain;
+	}
+	$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/domains?type=dedicated&_expand=true', 'GET', NULL, $api_key);
+	foreach ($json_output[entities] as $dedicated_domain) {
+		$clickmeter_domains[] = $dedicated_domain;
+	}
+
+	//if default is null, take first returned domain as default domain
+	if($default_domainId == null){
+		WPClickmeter::store_option("clickmeter_default_domainId", $wordpress_domain_id);
+		WPClickmeter::store_option("clickmeter_default_domainName", $blog_name);
+	} 
+
+	//CAMPAIGNS
+	$default_campaignId = WPClickmeter::get_option("clickmeter_default_campaignId_links");
+	if($default_campaignId==null) {
+		$default_campaignId = WPClickmeter::get_option('clickmeter_TLcampaign_id');
+		WPClickmeter::store_option("clickmeter_default_campaignId_links", $default_campaignId);
+	}
+	if($_POST['campaign_list']!=null){
+		$default_campaignId = $_POST['campaign_list'];
+		WPClickmeter::store_option("clickmeter_default_campaignId_links", $default_campaignId);
+	}
+
+	$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/groups?status=active&_expand=true', 'GET', NULL, $api_key);
+	$groups = $json_output[entities];
+
+
+	//CONVERSIONS
+	if($_POST['first_conversion_list']!=null){
+		$default_firstConv_links = $_POST['first_conversion_list'];
+		WPClickmeter::store_option("clickmeter_default_firstConv_links", $default_firstConv_links);	
+	}
+	$default_firstConv_links = WPClickmeter::get_option("clickmeter_default_firstConv_links");
+
+
+	if($_POST['second_conversion_list']!=null){
+		$default_secondConv_links = $_POST['second_conversion_list'];
+		WPClickmeter::store_option("clickmeter_default_secondConv_links", $default_secondConv_links);
+	}
+	$default_secondConv_links = WPClickmeter::get_option("clickmeter_default_secondConv_links");
+
+	//REDIRECTION TYPE
+	if($_POST['redirection_type_list']!=null){
+		$default_redirection_type = $_POST['redirection_type_list'];
+		WPClickmeter::store_option("clickmeter_default_redirection_type", $default_redirection_type);
+	}
+	$default_redirection_type = WPClickmeter::get_option("clickmeter_default_redirection_type");
+
+
+
+	//LINK CLOAKING
+	if($_POST['link_cloak_flag']=="true"){
+		WPClickmeter::store_option("link_cloak_flag", 1);
+	}
+	if($_POST['link_cloak_flag']=="false"){
+		WPClickmeter::store_option("link_cloak_flag", 0);
+	}
+	$link_cloak_flag = WPClickmeter::get_option("link_cloak_flag");
+
+	//404 TRACKING
+	if($_POST['track_404_flag']=="true"){
+		WPClickmeter::store_option("clickmeter_track_404_flag", 1);
+		//create 404 tracking link
+		$clickmeter_404_tl = WPClickmeter::get_option("clickmeter_404_tl");
+		$url_404_name = "wordpress-404";
+		$url_404 = $_POST['custom_404_page'];
+		WPClickmeter::store_option("clickmeter_track_404_url", $url_404);
+		$url_404_name = WPClickmeter::generateRandomString() ."-". $url_404_name;
+		if($clickmeter_404_tl==null){
+			$body=array('type'=> 0,
+				'title'=> "WordPress 404",
+				'groupId'=> $group_id_404_reports,
+				'name'=> $url_404_name,
+				'typeTL'=>array('domainId'=> WPClickmeter::get_option("clickmeter_default_domainId"), "redirectType"=> $default_redirection_type, 'url'=> $url_404)
+			);
+			$created_link = WPClickmeter::api_request('http://apiv2.clickmeter.com/datapoints','POST', json_encode($body), $api_key);
+			$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/datapoints/'.$created_link[id], 'GET', NULL, $api_key);
+			WPClickmeter::store_option("clickmeter_404_tlID", $json_output[id]);
+			WPClickmeter::store_option("clickmeter_404_tl", $json_output[trackingCode]);
+		}else{
+			$tl_404_id = WPClickmeter::get_option("clickmeter_404_tlID");
+			$json_output = WPClickmeter::api_request('http://apiv2.clickmeter.com/datapoints/'.$tl_404_id,'GET', null, $api_key);
+			if(strcasecmp($json_output[typeTL][url],$url_404)==0){
+				WPClickmeter::store_option("clickmeter_404_tlID", $json_output[id]);
+				WPClickmeter::store_option("clickmeter_404_tl", $json_output[trackingCode]);
+			}else{
+				$body=array('type'=> 0,
+					'title'=> $json_output[title],
+					'groupId'=> $json_output[groupId],
+					'name'=> $json_output[name],
+					'typeTL'=>array('domainId'=> $json_output[typeTL][domainId], "redirectType"=> $default_redirection_type, 'url'=> $url_404)
+				);
+				$updated_link = WPClickmeter::api_request('http://apiv2.clickmeter.com/datapoints/'.$tl_404_id,'POST', json_encode($body), $api_key);
+				WPClickmeter::store_option("clickmeter_404_tlID", $updated_link[id]);
+				WPClickmeter::store_option("clickmeter_404_tl", $json_output[trackingCode]);
+			}
+		}
+	}
+	if($_POST['track_404_flag']=="false"){
+		WPClickmeter::store_option("clickmeter_track_404_flag", 0);
+	}
+	$track_404_flag = WPClickmeter::get_option("clickmeter_track_404_flag");
+	$url_404 = WPClickmeter::get_option("clickmeter_track_404_url");
+
+	global $wp;
+
+	$workInProgress = WPClickmeter::get_option('clickmeter_workinprogress_flag');
+
+//phpinfo();
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,148 @@
+=== Plugin Name ===
+Contributors: ClickMeter
+Tags: 301 redirect, admin, advertisers, affiliates, Agencies, analytic, analytics, analyze, automation, bitly, click, clickmeter, clicks, cloak, cloaking, custom url, ga, google analytics, link management, link monitor, link tracker, link tracking, marketing, mask, masking, monitor, online users, page, pages, pageviews, permalink, plugin, Post, posts, publishers, qr code, redirect, reports, short url, shorten, shortlink, shorturl, spiders, statistics, stats, tinyurl, track, tracker, tracking, url shortener, views, widget, wordpress, world map, wp.gl, wp.me.
+Requires at least: 3.5.0
+Tested up to: 4.0
+Stable tag: 1.2.4.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Donate link: http://clickmeter.com/pricing-signup
+
+Customizable Link Shortener combined with Powerful Real-Time Analytics. Create short tracking links and track everything about your visitors.
+
+== Description ==
+The ClickMeter Link Shortener and Analytics plugin‏ is dedicated to marketing Agencies, Affiliates, Publishers and Advertisers. This powerful all-in-one plugin allows you to immediately optimize your web-marketing actions:
+
+- <strong>Custom Domain link shortner</strong>:
+Shorten every link inside and outside of your WordPress site using your own domain name (as opposed to using tinyurl.com, goo.gl, bit.ly, or any other link shortner service). Using a dedicated domain, short url’s will increase brand awareness, not look spammy and will not be blacklisted.
+
+- <strong>Advanced url redirect</strong>:
+Using the advanced redirection options you will be able to redirect users where they yield the best conversion rates, be able to protect your affiliate codes and create safe links for SEO.
+
+- <strong>Actionable analytics</strong>:
+Understand which posts or pages are the most viewed, which posts or pages lead to the most valuable conversions and why.  You have the ability to see all the results in one easy-to read report or if you prefer you can drill-down to view every single click or conversion.
+
+- <strong>Link Monitor</strong>:
+ Discover broken links and redirect them to a custom page, detect click fraud activities, blacklisted landing pages and other wrong server activities.
+ 
+Our WordPress plugin is a powerful and extremely scalable tool because it is based on ClickMeter Engines API and database clusters so it will not load your WP database and server. You will be able to create as many links and track as many posts and pages as you need.
+
+To use this all-in-one plugin you need a [ClickMeter Account](http://clickmeter.com/pricing-signup). You can subscribe for free or choose a premium plan depending on how many events you need to track or short links you need to create.
+
+= Features =
+- Create <strong>short links</strong> using your own domain name
+- Track <strong>meaningful insights</strong> into your website’s visitors including: the number of unique and non-unique visitors; human or robots; nation; town; IP address;
+- Mobile or desktop; operating system, language.
+- Track the number and value of <strong>conversions</strong> that were lead by your WP blog: opt-ins, purchase, subscriptions, downloads, etc…
+- Track important details about <strong>traffic sources:</strong> referrer, keywords, parameters.
+- Advanced <strong>redirect options</strong> including redirect by nation, by language, by unique visitors, click cap, custom timeframe.
+- <strong>Protect your affiliate code</strong> with advanced cloaking.
+- <strong>Monitor your links</strong> and protect them from click fraud actions, 400 and 500 server errors, blacklists.
+- Track <strong>404 errors</strong>, page not found, and redirect your “lost” visitors to a customizable landing page.
+- Other features: Real-time, hosted engines and databases, multilanguage dashboard, Google analytics compatible; SEO safe (301 or 307) redirect
+
+== Installation ==
+
+<h4>Install plugin</h4>
+You can install ClickMeter Link Shortener and Analytics in the following ways:
+<ul>
+<li> Activate the plugin through the 'Plugins' menu in WordPress (Plugins > Add New)
+<li> Upload 'clickmeter-link-shortener-and-analytics.zip' to the '/wp-content/plugins/' directory
+</ul>
+Once activated, enter your [ClickMeter API key](http://mybeta.clickmeter.com/api-key).
+
+<h4>Update plugin</h4>
+Every available update will be shown on the Dashboard Panel with an alert on the Plugin's menu title, and on the Plugin List.
+
+<h4>Uninstall plugin</h4>
+To uninstall ClickMeter Link Shortener and Analytics:
+<ol>
+<li> Click on “Remove API-Key” button in the main page of the plugin (Settings menù).</li>
+<li> Wait for the delete operation to complete.</li>
+<li> Go to plugins directory in the WordPress admin dashboard.</li>
+<li> Click on "deactivate" button to turn off the plugin.</li>
+<li> Click on "delete" button to complete the uninstall process.</li>
+</ol>
+<br><br>If you need more information about installing, updating or uninstalling plugins, take a look to the official [WordPress guide](http://codex.wordpress.org/Managing_Plugins).
+
+== Frequently Asked Questions ==
+You'll find updated FAQs on [ClickMeter Knowledge Base](http://support.clickmeter.com/forums/21156669-WordPress-plugin).
+
+== Screenshots ==
+1. Create your ClickMeter Account (free or premium) in seconds. Settings are very easy and available in the same page. 
+2. Every page or post contains a quick access to main actions and reports.
+3. How to create a dedicated short link.
+4. Fast access to dedicated short link and reports.
+5. List of tracked posts.
+6. List, in real-time, of all the visitors on your posts and short links.
+7. World map with all the people viewing your posts.
+8. QR-code from one of the shortened links.
+
+== Changelog ==
+
+= 1.2.4.2 =
+<ul>
+<li>Bugfix for 404 redirection links</li>
+<li>Changed default redirection type from 301 (permanent) to 307(temporary)</li>
+</ul>
+
+= 1.2.4.1 =
+<ul>
+<li>Patch to tracking links redirection fix of the previous of version 1.2.4</li>
+</ul>
+
+= 1.2.4 =
+<ul>
+<li>Enabled parameters passing for tracking links created with WP plugin</li>
+<li>Hide menu entry for other users than administrators</li>
+<li>Fixed post's tracking links redirection loop</li>
+<li>Minor bugfix</li>
+</ul>
+
+= 1.2.3 =
+<ul>
+<li>Changed tracking links table order (last created first)</li>
+<li>Fixed available datapoints counter</li>
+<li>Patch to clickmeter's logo</li>
+<li>Other view optimizations</li>
+<li>Minor bugfix</li>
+</ul>
+
+= 1.2.2 =
+<ul>
+<li>Added clickmeter logo</li>
+<li>Changed input text box to textarea for destination URLs</li>
+<li>Other view optimizations</li>
+<li>Minor bugfix</li>
+</ul>
+
+= 1.2.1 =
+<ul>
+<li>Changed delete TL popup</li>
+<li>Changed some labels</li>
+<li>Changed number of link in the list</li>
+<li>Other bugfix</li>
+</ul>
+
+= 1.2.0 =
+<ul>
+<li>New system to manage operations involving lots of posts/pages</li>
+<li>Removed "on start" tracking pixel creation</li>
+<li>New "safe" uninstalling system</li>
+<li>Compatibility tested upon WordPress 4.0</li>
+<li>New copy-to-clipboard dialog</li>
+<li>Other bugfix</li>
+</ul>
+
+= 1.1.0 =
+<ul>
+<li>Multiple WordPress blogs installation</li>
+<li>Campaing names changed in "your_wordpress_blog_name".com-xxxxxx</li>
+<li>New conversions management: select to associate conversion only to posts, pages or both of them</li>
+<li>Added WordPress database cleaning after uninstalling</li>
+<li>Added cloaking with URL encryption for tracking links</li>
+<li>Minor bugfix</li>
+</ul>
+
+= 1.0.0 =
+First version release.
